def1e58fc663bcdf9a34df1f38028a832aca0a33
Ling <166816238+XueL1ng@users.noreply.github.com>
2026-07-31 20:34:20 +0800

    feat(homepage): 增删一些自定义主页预设 (#3373)
    * feat(homepage): 同步三个新的自定义主页预设
    
    * 移除几个上游已经移除的主页（他们基本都不可用了）
    
    * 漏了一个并修正了一些 item 编号
    
    * 版本更新兜个底
    
    * 忘记 i18n 了
    
    * 改个默认值
    
    Upstream-Commit: 669c37f31753cf09f9cde505440293e64f016959
    Upstream-URL: https://github.com/PCL-Community/PCL-CE/commit/669c37f31753cf09f9cde505440293e64f016959

--- Plain Craft Launcher 2/FormMain.xaml.cs
+++ Plain Craft Launcher 2/FormMain.xaml.cs
@@ -294,6 +294,14 @@ public partial class FormMain
         // 输出更新日志
         if (lastVersion is null) return;
         ShowUpdateLog();
+
+        // 重置自定义主页配置（预设序号因增删项发生变化；对应 CE versionCode < 521）
+        if (lastVersion < LauncherBaseVersion.Parse("2026.07.9") &&
+            Config.Preference.Homepage.SelectedPreset >= 3)
+        {
+            Config.Preference.Homepage.SelectedPreset = 0;
+            ModMain.MyMsgBox(Lang.Text("Main.HomepageReset.Content"), Lang.Text("Main.HomepageReset.Title"));
+        }
     }
 
     private void DowngradeSub(LauncherBaseVersion lastVersion)
